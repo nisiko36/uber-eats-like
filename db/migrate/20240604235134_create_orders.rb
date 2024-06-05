cf560c5ab722f81b0caebdfdc06d584455e9ffe2
Koki Nishimura <test@mail.com>
2024-06-05 09:34:32 +0900

    db::migrate

--- db/migrate/20240604235134_create_orders.rb
+++ db/migrate/20240604235134_create_orders.rb
@@ -2,6 +2,8 @@ class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
 
+      t.integer :total_price, null: false, default: 0
+
       t.timestamps
     end
   end
