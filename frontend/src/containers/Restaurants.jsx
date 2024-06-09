07b249a6fecda6478e75fcef2ffada179993186e
Koki Nishimura <nisikou9@icloud.com>
2024-06-09 16:44:18 +0900

    add src/containers/Restaurant.jsx useEffect

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
+
+    useEffect(() => {
+        fetchRestaurants()
+        .then((data) =>
+            console.log(data)
+        )
+    },[])
+
     return (
         <div>
             レストラン一覧
