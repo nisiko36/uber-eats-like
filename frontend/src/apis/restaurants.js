076690f9d426ce53deae6c2e13239bfcfa65c455
Koki Nishimura <nisikou9@icloud.com>
2024-06-09 16:30:47 +0900

    create,add APIを叩く関数を定義

---
+++ frontend/src/apis/restaurants.js
@@ -0,0 +1,10 @@
+import { restaurantsIndex } from "../urls"
+import axios from 'axios';
+
+export const fetchRestaurants =() => {
+    return axios.get(restaurantsIndex)
+        .then(res => {
+            return res.data
+        })
+        .catch((e) => console.error(e))
+}
