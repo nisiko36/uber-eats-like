a6381c99a9bfe715848f82e112f5fb00765a6aa0
Koki Nishimura <nisikou9@icloud.com>
2024-06-11 18:19:02 +0900

    edit Restaurants.jsx APIのデータを画面に表示する

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useReducer,useEffect } from 'react';
 import styled from 'styled-components';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
 
+import {
+    initialState,
+    restaurantsActionTypes,
+    restaurantsReducer,
+} from '../reducers/restaurants';
+
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+
 
 const HeaderWrapper = styled.div`
     display: flex;
@@ -27,10 +34,18 @@ const MainCover = styled.img`
 `;
 
 export const Restaurants = () => {
+    const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
     useEffect(() => {
+        dispatch({ type: restaurantsActionTypes.FETCHING });
         fetchRestaurants()
         .then((data) =>
-            console.log(data)
+            dispatch({
+                type: restaurantsActionTypes.FETCH_SUCCESS,
+                payload: {
+                    restaurants: data.restaurants
+                }
+            })
         )
     },[])
 
@@ -40,8 +55,16 @@ export const Restaurants = () => {
                 <MainLogoImage src={MainLogo} alt="main logo"/>
             </HeaderWrapper>
             <MainCoverImageWrapper>
-                <MainCover src={MainCoverImage} alt='main cover' />
+                <MainCover src={MainCoverImage} alt='main cover'/>
             </MainCoverImageWrapper>
+
+            {
+                state.restaurantsList.map(restaurant =>
+                    <div key ={restaurant.id}>
+                        {restaurant.name}
+                    </div>
+                )
+            }
         </div>
     )
 }
