517520ff063d173a017af29a1ed063f3c0c3ef20
Koki Nishimura <nisikou9@icloud.com>
2024-07-06 16:50:32 +0900

    edit orders_controller.rb

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -16,7 +16,7 @@ module Api
             private
 
             def total_price(posted_line_foods)
-                posted_line_foods.sum{|line_food| line_food.total_amount } + posted_line_foods
+                posted_line_foods.sum{|line_food| line_food.total_amount } + posted_line_foods.first.restaurant.fee
             end
         end
     end
